2401773f1818c59a910d50091823dfc1418e127c
mo3az96 <mo3az.abdellatif@gmail.com>
2025-08-19 09:59:08 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,71 @@
+$(window).on("load", function () {
+  $("body").removeClass("overflow");
+});
+$(document).ready(function () {
+  /***** Menu *****/
+  if ($(window).width() <= 767) {
+    $(".menu-btn").click(function () {
+      $(".header-nav").addClass("active");
+      $("body").addClass("overflow");
+      $(".overlay").fadeIn();
+    });
+    $(".close-btn,.overlay").click(function () {
+      $(".header-nav").removeClass("active");
+      $("body").removeClass("overflow");
+      $(".overlay").fadeOut();
+    });
+  }
+
+  /***** main slider *****/
+  var mainSwiper = new Swiper(".main-slider .swiper", {
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+    },
+    pagination: {
+      el: ".main-slider .swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  /***** testimonials slider *****/
+  var testimonialsSwiper = new Swiper(".testimonials-slider .swiper", {
+    loop: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 15,
+      },
+      992: {
+        slidesPerView: 2,
+        spaceBetween: 15,
+      },
+      1200: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+    },
+    pagination: {
+      el: ".testimonials-slider .swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  /***** Scroll To Top *****/
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 500) {
+      $(".toTop").addClass("showToTop");
+    } else {
+      $(".toTop").removeClass("showToTop");
+    }
+  });
+
+  $(".toTop").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 400);
+  });
+});
